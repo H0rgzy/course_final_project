575e2c1c9cff16a2c242e99d23a34534395dac0e
H0rgzy <peter.horgan14@gmail.com>
2022-06-22 19:32:58 +0100

    fixed log in issues

--- course_final_project/forms.py
+++ course_final_project/forms.py
@@ -7,7 +7,7 @@ class RegistrationForm(FlaskForm):
     username = StringField("Username", validators= [DataRequired()])
     email = EmailField("Email address", validators= [DataRequired(), Email()])
     password = PasswordField("Password", validators= [DataRequired()])
-    password2 = PasswordField("Repeat Password", validators= [DataRequired(), EqualTo("Password")])
+    password2 = PasswordField("Repeat Password", validators= [DataRequired(), EqualTo("password")])
     submit = SubmitField("Register")
 
 class SigninForm(FlaskForm):
--- course_final_project/routes.py
+++ course_final_project/routes.py
@@ -3,6 +3,7 @@ from flask import render_template, redirect, url_for, request, flash
 from flask_login import login_required, login_user, current_user
 from forms import RegistrationForm, SigninForm
 from models import User, Visit
+from werkzeug.urls import url_parse
 
 
 @app.route("/")
@@ -12,7 +13,7 @@ def index():
 @app.route("/registration", methods=["GET", "POST"])
 def registration():
     form = RegistrationForm()
-    if form.validate_on_submit():
+    if request.method == 'POST':
         user = User(username = form.username.data, email = form.email.data)
         user.set_password(form.password.data)
         db.session.add(user)
@@ -28,16 +29,22 @@ def signin():
     if form.validate_on_submit():
         user = User.query.filter_by(username = form.username.data).first()
         if user and user.check_password(form.password.data):
+            print('success')
             login_user(user)
-            next_page = request.args.get("next")
-            return redirect(next_page) if next_page else redirect(url_for("account", _external= True, _scheme="https"))
+            
+            next = request.args.get('next')
+            if not next or url_parse(next).netloc != '':
+                next = url_for('account')
+                return redirect(next)
+
+            return redirect(next or url_for("account"))
+        
     return render_template("signin.html", form=form)
 
 
 
-# @app.route("/account/<user>")
-# @login_required
-# def account(username):
-#     user = User.query.filter_by(username=username).first_or_404()
-#     return render_template("account.html", user=user)
+@app.route("/account")
+@login_required
+def account():
+    return "you are logged in"
 
